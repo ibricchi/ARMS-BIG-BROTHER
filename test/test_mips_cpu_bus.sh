249a2ce602deb3d73a5a71bbc4a46faa4faa9f78
nicholas-p1 <kontakt@nicholaspfaff.ch>
2020-11-26 17:47:01 +0000

    Pass SOURCE_DIRECTORY to run_one_testcase.sh

--- test/test_mips_cpu_bus.sh
+++ test/test_mips_cpu_bus.sh
@@ -11,6 +11,6 @@ for i in ${TESTCASES} ; do
     TEST_INSTRUCTION="$(grep -oE ^${INSTRUCTION} <<< ${TESTNAME})"
     
     if [[ "$INSTRUCTION" == "$TEST_INSTRUCTION" ]] || [[ "$INSTRUCTION" == "ALL" ]] ; then
-        ./run_one_testcase.sh ${CPU_VARIANT} ${TESTNAME}
+        ./run_one_testcase.sh ${SOURCE_DIRECTORY} ${CPU_VARIANT} ${TESTNAME}
     fi
 done

ba36f3ef3029e9202d40258dfa48ff5211e3c43b
nicholas-p1 <kontakt@nicholaspfaff.ch>
2020-11-26 18:34:25 +0000

    Implement 'run_one_testcase.sh'
    This is not 100% finished yet as it has dependencies on paths of not yet existing components - assembler, reference simulator

--- test/run_one_testcase.sh
+++ test/run_one_testcase.sh
@@ -1,6 +1,50 @@
 #!bin/bash
 
-# Runs one testcase as specified by the input arguments
-# Input arguments: 
-# CPU variant (Bus or Harvard)
-# testcase
+set -eou pipefail
+
+SOURCE_DIRECTORY="$1"
+CPU_VARIANT="$2"
+TESTCASE="$3"
+
+ASSEMBLER_PATH="./" # SPECIFY THIS!
+REFERENCE_SIMULATOR_PATH="./" # SPECIFY THIS!
+
+>&2 echo "Test MIPS ${CPU_VARIANT} using test-case ${TESTCASE}"
+
+>&2 echo "  1 - Assembling test case"
+ASSEMBLER_PATH < ./assembly/${TESTCASE}.asm > ./binary/${TESTCASE}.hex
+
+>&2 echo "  2 - Compiling test-bench"
+iverilog -g 2012 \
+   SOURCE_DIRECTORY/*.v \ # maybe need to add RAM file here (depending on implementation)
+   -s mips_cpu_${VARIANT}_tb \ # we are including all .v files in SOURCE_DIRECTORY and hence need to be exact about which VARIANT to test here
+   -o ./simulator/mips_cpu_${VARIANT}_tb_${TESTCASE}
+
+>&2 echo "  3 - Running test-bench"
+set +e
+./simulator/mips_cpu_${VARIANT}_tb_${TESTCASE} > ./output/mips_cpu_${VARIANT}_tb_${TESTCASE}.out
+RESULT=$?
+set -e
+
+if [[ "${RESULT}" -ne 0 ]] ; then
+   echo "   MIPS_${VARIANT}, ${TESTCASE}, FAIL"
+   exit
+fi
+
+>&2 echo "  4 - Running reference simulator"
+set +e
+# might need to modify the input to REFERENCE_SIMULATOR_PATH, depending on how REFERENCE_SIMULATOR is implemented
+REFERENCE_SIMULATOR_PATH < ./binary/${TESTCASE}.hex > ./reference/${TESTCASE}.out
+set -e
+
+>&2 echo "  5 - Comparing output"
+set +e
+diff -w ./reference/${TESTCASE}.out ./output/mips_cpu_${VARIANT}_tb_${TESTCASE}.out
+RESULT=$?
+set -e
+
+if [[ "${RESULT}" -ne 0 ]] ; then
+   echo "   MIPS_${VARIANT}, ${TESTCASE}, FAIL"
+else
+   echo "   MIPS_${VARIANT}, ${TESTCASE}, PASS"
+fi

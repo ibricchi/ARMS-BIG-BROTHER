542b4e8fdcb1751b7d18035ffae430860603d944
set <set>
2020-11-26 19:26:35 +0000

    Fix bugs in Label and Instruction scanning

--- test/tools/assembler/assembler.cpp
+++ test/tools/assembler/assembler.cpp
@@ -2,20 +2,25 @@
 #include <vector>
 #include <unordered_map>
 
-#include <scanner.h>
+#include "scanner.h"
+#include "scanner.cpp"
 
 using namespace std;
 
 int main(){
+    Scanner scanner = Scanner();
     unordered_map<string, uint32_t> labels{};
 
     string line;
-    getline(cin, line);
-    for(; !cin.fail(); getline(cin, line)){
-        for(auto it = line.begin(); it != line.end(); it++){
-            
-        }
+    bool scanError = false;;
+    for(getline(cin, line); !cin.fail(); getline(cin, line)){
+        scanner.scanLine(line);
+        scanError |= scanner.error;
+        scanner.error = false;
     }
+
+    int a = 1;
+    a = a + a;
 
     vector<uint32_t> mem{};
 }
--- test/tools/assembler/scanner.cpp
+++ test/tools/assembler/scanner.cpp
@@ -20,10 +20,9 @@ Scanner::Scanner(){
 }
 const unordered_map<string, uint8_t> Scanner::op_map = {
     //{OP_NAME, Special, OPCODE, RS, RT, RD, IMM}
-    {"ADD",     0b1000000},
     {"ADDI",    0},
     {"ADDIU",   0},
-    {"ADDU",    0},
+    {"ADDU",    0b100001},
     {"AND",     0},
     {"ANDI",    0},
     {"BEQ",     0},
@@ -32,24 +31,33 @@ const unordered_map<string, uint8_t> Scanner::op_map = {
 };
 
 void Scanner::reset(){
+    line = 0;
+    memLine = 0;
+    labels = vector<Token>();
     tokens = vector<Token>();
+    error = false;
 }
 
 void Scanner::errorMsg(string msg){
     error = true;
     cerr << "[Line " << line << "] " << msg << endl;
 }
-void Scanner::expectWhiteSpace(string::iterator it, string::iterator end){
+void Scanner::expectWhiteSpace(string::iterator& it, string::iterator end){
     bool allSpaces = true;
     string msg = "";
     while(it != end){
         allSpaces &= (*it == ' ');
         msg += *it;
+        it++;
     }
     if(!allSpaces){
         error = true;
         errorMsg("Expected whitespace, found '" + msg + "' instead.");
     }
+}
+void Scanner::skipWhiteSpace(string::iterator& it, string::iterator end){
+    while(*it == ' ' && it != end) it++;
+    if(it == end) errorMsg("Expected more parameters, reached end of line instead.");
 }
 
 inline static bool is_alpha(char c){
@@ -68,7 +76,7 @@ inline static bool is_alpha_numeric(char c){
     return is_alpha(c) || is_numeric(c) || (c == '_') ;
 }
 
-uint32_t Scanner::const_line(string::iterator it, string::iterator end){
+uint32_t Scanner::const_line(string::iterator& it, string::iterator end){
     string str_val = "";
     str_val += *it;
 
@@ -89,7 +97,7 @@ uint32_t Scanner::const_line(string::iterator it, string::iterator end){
             else return stoi(str_val);
         }
         else{
-            string it_str = "" + *it;
+            string it_str = string() + *it;
             errorMsg("Unexpected character '" + it_str + "' in constant.");
             return 0;
         }
@@ -116,7 +124,7 @@ uint32_t Scanner::const_line(string::iterator it, string::iterator end){
                 expectWhiteSpace(it, end);
                 if(error) return 0;
             }else{
-                string it_str = "" + *it;
+                string it_str = string() + *it;
                 errorMsg("Unexpected character '" + it_str + "' in constant.");
                 return 0;
             }
@@ -146,43 +154,52 @@ uint32_t Scanner::const_line(string::iterator it, string::iterator end){
     }
 
 }
-uint8_t Scanner::read_reg(string::iterator it, string::iterator end, bool comma_terminated){
+uint8_t Scanner::read_reg(string::iterator& it, string::iterator end, bool comma_terminated){
     if(*it != '$'){
         errorMsg("Expected a register.");
         return 0;
     }
     it++;
-    if(it != end){
+    if(it == end){
         errorMsg("Expected register number, reached end of line instead.");
         return 0;
     }
     if(!is_numeric(*it)){
-        string it_str = "" + *it;
+        string it_str = string() + *it;
         errorMsg("Expected integer found '" + it_str + "' instead.");
         return 0;
     }
     string reg_str = "";
     if(is_numeric(*it)) reg_str += *it;
     it++;
-    if(comma_terminated ? *it == ',' : (it == end || *it == ' ')) return stoi(reg_str);
+    if(comma_terminated ? *it == ',' : (it == end || *it == ' ')){
+        if(comma_terminated) it++;
+        return stoi(reg_str);
+    }
     if(!is_numeric(*it)){
-        string it_str = "" + *it;
+        string it_str = string() + *it;
         errorMsg("Expected integer found '" + it_str + "' instead.");
         return 0;
     }
     reg_str += *it;
     it++;
     if(comma_terminated ? *it != ',' : (it != end && *it != ' ')){
-        string it_str = "" + *it;
+        string it_str = string() + *it;
         if(comma_terminated)
             errorMsg("Expected comma, found '" + it_str + "' instead.");
         else
             errorMsg("Expected space or end of line, found '" + it_str + "' instead.");
         return 0;
     }
-    return stoi(reg_str);
+    if(comma_terminated) it++;
+    uint8_t reg_num = stoi(reg_str);
+    if(reg_num > 31){
+        errorMsg("Register numbers range between 0-31, found '" + to_string(reg_num) + "'.");
+        return 0;
+    }
+    return reg_num;
 }
-uint32_t Scanner::instr_line(string instr, string::iterator it, string::iterator end){
+uint32_t Scanner::instr_line(string instr, string::iterator& it, string::iterator end){
     if(op_map.find(instr) == op_map.end()){
         errorMsg("Unkown instruction '" + instr + "'.");
         return 0;
@@ -192,36 +209,35 @@ uint32_t Scanner::instr_line(string instr, string::iterator it, string::iterator
     switch (op)
     {
     // 3 register expressions
-    case 0b10001: // ADD
+    case 0b100001:{ // ADD
         uint8_t rs = read_reg(it, end, true);
         if(error) return 0;
         if(it == end){
             errorMsg("Not enough parameters passed to instruction.");
             return 0;
         }
+        skipWhiteSpace(it, end);
+        if(error) return 0;
         uint8_t rt = read_reg(it, end, true);
         if(error) return 0;
         if(it == end){
             errorMsg("Not enough parameters passed to instruction.");
             return 0;
         }
+        skipWhiteSpace(it, end);
+        if(error) return 0;
         uint8_t rd = read_reg(it, end, false);
         if(error) return 0;
-        if(it == end){
-            errorMsg("Not enough parameters passed to instruction.");
-            return 0;
+        if(it != end){
+            expectWhiteSpace(it, end);
+            if(error) return 0;
         }
-        expectWhiteSpace(it, end);
-        if(error) return 0;
         out |= rs;
         out = out << 5 | rt;
         out = out << 5 | rd;
         out = out << 10 | op;
         break;
-        
-    // imediate expressions
-    // case 0b001000: // ADDIU
-    //     break;
+    }
     default:
         errorMsg("Instruction '" + instr + "' has not yet been implemented, but should exist.");
         return 0;
@@ -234,7 +250,7 @@ void Scanner::scanLine(string in){
 
     auto it = in.begin();
     // if first thing is a constant
-    if(!is_numeric(*it)){
+    if(is_numeric(*it)){
         tokens.push_back({"CONST", const_line(it, in.end())});
         line++;
         memLine++;
@@ -243,7 +259,7 @@ void Scanner::scanLine(string in){
     for(; it != in.end(); it++){
         // if first thing is an instr
         if(str.size() > 0 && (*it) == ' '){
-            it++;
+            skipWhiteSpace(it, in.end());
             tokens.push_back({str, instr_line(str, it, in.end())});
             memLine++;
             line++;

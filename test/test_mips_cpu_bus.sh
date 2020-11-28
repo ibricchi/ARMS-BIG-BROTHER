4045e119ad1bed4d07e15626ff0c401272eb8796
nicholas-p1 <kontakt@nicholaspfaff.ch>
2020-11-28 15:01:44 +0000

    Modify 'test_mips_cpu_bus.sh' to test general c-programs

--- test/test_mips_cpu_bus.sh
+++ test/test_mips_cpu_bus.sh
@@ -5,6 +5,7 @@ INSTRUCTION="${2:-ALL}" # Run all testcases if no instruction is specified
 CPU_VARIANT="bus"
 
 TESTCASES="./assembly/*.asm"
+C_TEST_FILES="./test_program/*.c"
 
 for i in ${TESTCASES} ; do
     TESTNAME=$(basename ${i} .asm)
@@ -12,5 +13,13 @@ for i in ${TESTCASES} ; do
     
     if [[ "$INSTRUCTION" == "$TEST_INSTRUCTION" ]] || [[ "$INSTRUCTION" == "ALL" ]] ; then
         ./run_one_testcase.sh ${SOURCE_DIRECTORY} ${CPU_VARIANT} ${TESTNAME}
+    fi
+done
+
+for i in ${C_TEST_FILES} ; do
+    TESTNAME=$(basename ${i} .c)
+    
+    if [[ "$INSTRUCTION" == "ALL" ]] || [[ "$INSTRUCTION" == "c" ]] ; then
+        ./run_one_testprogram.sh ${SOURCE_DIRECTORY} ${CPU_VARIANT} ${TESTNAME}
     fi
 done

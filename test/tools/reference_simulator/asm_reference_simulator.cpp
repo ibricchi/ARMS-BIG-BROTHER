53660e974230a03f0175f3605b974273516936f0
nicholas-p1 <kontakt@nicholaspfaff.ch>
2020-12-07 17:43:15 +0000

    Add OR instruction to reference simulator

--- test/tools/reference_simulator/asm_reference_simulator.cpp
+++ test/tools/reference_simulator/asm_reference_simulator.cpp
@@ -120,6 +120,10 @@ uint32_t simulateMIPS(unordered_map<uint32_t, uint32_t> &memory, const uint32_t 
             }
             case 0b100101: // OR
             {
+                uint32_t dReg, sReg, tReg;
+                tie(dReg, sReg, tReg, ignore) = decodeArithmeticType(instruction);
+                regs[dReg] = regs[sReg] | regs[tReg];
+                pc += 4;
                 break;
             }
             case 0b101010: // SLT

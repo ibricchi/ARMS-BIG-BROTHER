9d12534ddb508843e1e834e81de99a1008c5ee5e
IBricchi <ignacio.bricchi@gmail.com>
2020-12-10 16:27:28 +0100

    Add comparison and shifts to ALU

--- rtl/alu.v
+++ rtl/alu.v
@@ -1,7 +1,8 @@
 module alu(
     input logic[31:0]  a,
     input logic[31:0]  b,
-    input logic[3:0]   alu_control,
+    input logic[4:0]   sa,
+    input logic[4:0]   alu_control,
 
     output logic[31:0] result,
     output logic       zero
@@ -9,19 +10,26 @@ module alu(
 
 always_comb begin
     case(alu_control)
-        4'b0000: result = a & b; //AND
-        4'b0001: result = a | b; //OR
-        4'b0010: result = a + b; //ADD
-        4'b0110: result = a - b; //SUB
-        4'b0111: result = (a < b) ? 1:0; //SLT (set on less than)
-        4'b1100: result = ~(a | b);//NOR
-        4'b1101: result = a ^ b; //XOR
+        5'b00000: result = a & b; //AND
+        5'b00001: result = a | b; //OR
+        5'b00010: result = a + b; //ADD
+        5'b00110: result = a - b; //SUB
+        5'b00111: result = a < b; //SLTU (set on less than)
+        5'b01000: result = $signed(a)<$signed(b); // SLT
+        5'b01001: result = b << sa; // SLL
+        5'b01010: result = b >>> sa; // SRA
+        5'b01011: result = b >> sa; // SRL 
+        5'b01100: result = ~(a | b);//NOR
+        5'b01101: result = a ^ b; //XOR
+        5'b01110: result = b << a; // SLLV
+        5'b01111: result = b >>> a; // SRAV
+        5'b10000: result = b >> a; // SRLV 
         
-        4'b1000: result = (a != b) ? 0:1; //BNE(set on equal)
-        4'b1001: result = ($signed(a) > 0) ? 0:1; //BGTZ(set on if a greater than 0)
-        4'b1010: result = ($signed(a) <= 0) ? 0:1; //BLEZ(set on if a less than or equal to 0)
-        4'b1011: result = ($signed(a) >= 0) ? 0:1; //BGEZ(set on if a greater than or equal to 0)
-        4'b1111: result = ($signed(a) < 0) ? 0:1; //BLTZ(set on if a less than 0)
+        5'b11000: result = (a != b) ? 0:1; //BNE(set on equal)
+        5'b11001: result = ($signed(a) > 0) ? 0:1; //BGTZ(set on if a greater than 0)
+        5'b11010: result = ($signed(a) <= 0) ? 0:1; //BLEZ(set on if a less than or equal to 0)
+        5'b11011: result = ($signed(a) >= 0) ? 0:1; //BGEZ(set on if a greater than or equal to 0)
+        5'b11111: result = ($signed(a) < 0) ? 0:1; //BLTZ(set on if a less than 0)
         default: result = 0;
     endcase
 end
--- test/assembly/mult_1.asm
+++ test/assembly/mult_1.asm
@@ -1,5 +1,4 @@
 ADDIU $a0, $a0, 10
 ADDIU $a1, $a1, 2
 MULT $a0, $a1
-MFLO $v0
 JR $ra

f5e9ef8170c7ad1f7cc48ccbf31a60f0d401eeab
hz2419 <hz2419@ic.ac.uk>
2020-12-10 15:43:12 +0800

    increased size of ALUop, add BranchzFunc for BLTZ and OTHER_BRANCHZ

--- rtl/alu_control.v
+++ rtl/alu_control.v
@@ -1,6 +1,7 @@
 module alu_control(
-    input logic[1:0] ALUOp,
+    input logic[3:0] ALUOp,
     input logic[5:0] FuncCode,
+    input logic[4:0] BranchzFunc,
 
     output logic[3:0] ALUCtrl
 );
@@ -20,13 +21,32 @@ always_comb begin
 end
 
 always_comb begin
+    case(BranchzFunc)
+        5'b00001: BranchCtrl = 4'b1011; //BGEZ
+        // 5'b10001: BranchCtrl = 4'b; //BGEZAL TODO
+        5'b00000: BranchCtrl = 4'b1111; //BLTZ
+        // 5'b10000: BranchCtrl = 4'b; //BLTZAL TODO     
+        default: BranchCtrl = 4'b0000;
+    endcase
+end
+
+always_comb begin
     case(ALUOp)
-        2'b00: ALUCtrl = 4'b0010; //add
-        2'b01: ALUCtrl = 4'b0110; //sub
-        2'b10: ALUCtrl = func; //R-type instructions
-        2'b11: ALUCtrl = 4'b0010; //add
+        4'b0000: ALUCtrl = 4'b0010; //add
+        4'b0001: ALUCtrl = 4'b0110; //sub
+        4'b0010: ALUCtrl = func; //R-type instructions
+        4'b0011: ALUCtrl = 4'b0010; //add
+        4'b0100: ALUCtrl = 4'b0000; //Bitwise AND
+        4'b0101: ALUCtrl = 4'b0001; //Bitwise OR
+        4'b0110: ALUCtrl = 4'b1101; //Bitwise XOR
+        4'b0111: ALUCtrl = 4'b0111; //SLT
+        4'b1000: ALUCtrl = 4'b1000; //bne
+        4'b1001: ALUCtrl = 4'b1001; //bgtz
+        4'b1010: ALUCtrl = 4'b1010; //blez
+        4'b1011: ALUCtrl = BranchCtrl; //BLTZ and OTHER_BRANCHZ instructions
         default: ALUCtrl = 0;
     endcase
 end
+       
 
 endmodule

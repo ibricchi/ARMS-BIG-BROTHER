a9e833608a19bcc59b9010dbf0b760936b5faa39
IBricchi <ignacio.bricchi@gmail.com>
2020-12-11 18:04:08 +0100

    remove debugging code

--- rtl/mips_cpu_bus.v
+++ rtl/mips_cpu_bus.v
@@ -24,28 +24,28 @@ end
 
 always_ff @(posedge clk) begin // on every clock cycle if waitrequest is low change state
     // debug code
-    $display("Instruction: ", instr, " PC: ", pc_out - 3217031168, " PC_IN: ", add_out - 3217031168, " Zero: ", zero, " Branch: ", branch);
+    // $display("Instruction: ", instr, " PC: ", pc_out - 3217031168, " PC_IN: ", add_out - 3217031168, " Zero: ", zero, " Branch: ", branch);
     if(!waitrequest) case(state)
         0: begin // HALT
             state <= 1;
             active <= 1;
-            $display("Entering FETCH STATE: ");
+            // $display("Entering FETCH STATE: ");
         end
         1: begin // FETCH
             state <= 2;
-            $display("Entering DECO STATE: ");
+            // $display("Entering DECO STATE: ");
         end
         2: begin // DECODE
             state <= 3;
-            $display("Entering EXEC1 STATE: ");
+            // $display("Entering EXEC1 STATE: ");
         end
         3: begin // EXEC1
             state <= 4;
-            $display("Entering EXEC2 STATE: ");
+            // $display("Entering EXEC2 STATE: ");
         end
         4: begin // EXEC2
             state <= 1;
-            $display("Entering FETCH STATE: ");
+            // $display("Entering FETCH STATE: ");
         end
     endcase
 end

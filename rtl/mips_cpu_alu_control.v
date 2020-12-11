5adff9a79ff7273c92f1b87893d2197253a638cf
IBricchi <ignacio.bricchi@gmail.com>
2020-12-11 17:16:17 +0100

    Fix branching to negative locations in Branch instructions

--- rtl/mips_cpu_alu_control.v
+++ rtl/mips_cpu_alu_control.v
@@ -41,17 +41,17 @@ end
 
 always_comb begin
     case(ALUOp)
-        4'b0000: ALUCtrl = 4'b0010; //add
-        4'b0001: ALUCtrl = 4'b0110; //sub
+        4'b0000: ALUCtrl = 5'b00010; //add
+        4'b0001: ALUCtrl = 5'b00110; //sub
         4'b0010: ALUCtrl = func; //R-type instructions
-        4'b0011: ALUCtrl = 4'b0010; //add
-        4'b0100: ALUCtrl = 4'b0000; //Bitwise AND
-        4'b0101: ALUCtrl = 4'b0001; //Bitwise OR
-        4'b0110: ALUCtrl = 4'b1101; //Bitwise XOR
-        4'b0111: ALUCtrl = 4'b0111; //SLT
-        4'b1000: ALUCtrl = 4'b1000; //bne
-        4'b1001: ALUCtrl = 4'b1001; //bgtz
-        4'b1010: ALUCtrl = 4'b1010; //blez
+        4'b0011: ALUCtrl = 5'b00010; //add
+        4'b0100: ALUCtrl = 5'b00000; //Bitwise AND
+        4'b0101: ALUCtrl = 5'b00001; //Bitwise OR
+        4'b0110: ALUCtrl = 5'b01101; //Bitwise XOR
+        4'b0111: ALUCtrl = 5'b00111; //SLT
+        4'b1000: ALUCtrl = 5'b11000; //bne
+        4'b1001: ALUCtrl = 5'b11001; //bgtz
+        4'b1010: ALUCtrl = 5'b11010; //blez
         4'b1011: ALUCtrl = BranchCtrl; //BLTZ and OTHER_BRANCHZ instructions
         default: ALUCtrl = 0;
     endcase

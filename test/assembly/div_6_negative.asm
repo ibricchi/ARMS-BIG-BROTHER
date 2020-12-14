fd494969a3e9ebc1cfbfd53a437c239d32419d2a
IBricchi <ignacio.bricchi@gmail.com>
2020-12-14 23:38:58 +0100

    Fix signed division and add test case

--- test/assembly/div_6_negative.asm
+++ test/assembly/div_6_negative.asm
@@ -1,5 +1,5 @@
-ADDIU $a0, $a0, 10
-SUBU $a1, $a1, $a0
-DIV $a0, $a1
-MFHI $v0
+ADDIU $a0, $a0, 10  # set a0 to 10
+SUBU $a1, $a1, $a0  # set a1 to -10
+DIV $a0, $a1        # a0/a1 = 10/-10
+MFHI $v0            #
 JR $ra

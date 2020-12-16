434c683b901e8b208fee09b86b7052b78f74270f
nicholas-p1 <kontakt@nicholaspfaff.ch>
2020-12-16 15:10:20 +0000

    Give permissions to scripts in top level test script

--- test/tools/build_tools.sh
+++ test/tools/build_tools.sh
@@ -4,7 +4,9 @@
 # It is responsible for all tools that might be necessary for 'test_mips_cpu_{bus/harvard}.sh' to run properly.
 
 # assembler
+chmod +x ./test/tools/assembler/build_assembler.sh
 ./test/tools/assembler/build_assembler.sh
 
 # reference simulator
+chmod +x ./test/tools/reference_simulator/build_reference_simulator.sh
 ./test/tools/reference_simulator/build_reference_simulator.sh

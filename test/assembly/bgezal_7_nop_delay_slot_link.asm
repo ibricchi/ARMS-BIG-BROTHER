ae87341e3e515e813e335f29d4f5db51a4078be5
IBricchi <ignacio.bricchi@gmail.com>
2020-12-16 11:23:20 +0100

    Add comments to bgezal_nop_delay_slot_link.asm

--- test/assembly/bgezal_7_nop_delay_slot_link.asm
+++ test/assembly/bgezal_7_nop_delay_slot_link.asm
@@ -1,9 +1,9 @@
-ADDIU $a0, $a0, 11
-SUBU $a1, $a1, $a0
-BGEZAL $a1, GreaterEqualZero
-NOP                       # Branch delay slot
-ADDIU $v0, $v0, 100
-GreaterEqualZero:
-ADDIU $v0, $v0, 3
-ADDU $v0, $v0, $ra
+ADDIU $a0, $a0, 11              # set a0 = 11
+SUBU $a1, $a1, $a0              # set a1 = 10
+BGEZAL $a1, GreaterEqualZero    # jump if a1 >= 0 which means it should jump and set L to 0xBFC00010
+NOP                             # Branch delay slot
+ADDIU $v0, $v0, 100             # skipped
+GreaterEqualZero:               # jump here
+ADDIU $v0, $v0, 3               # set v0 = 3
+ADDU $v0, $v0, $ra              # set v0 = 3 + 0xBFC00010 = 3217031187
 JR $zero
